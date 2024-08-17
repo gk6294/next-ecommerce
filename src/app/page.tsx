b044bce84d8946607d3aef47455fde5afd90f25d
barton05 <bartonchik05@gmail.com>
2024-08-17 18:54:09 +0800

    107 slider completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import Slider from "./components/Slider"
 
 const HomePage = () => {
   return (
-    <div className=''>
-      HomePage
-      <Slider/>
+    <div className="">
+      <Slider />
     </div>
-  )
+  );
 }
 
 export default HomePage
