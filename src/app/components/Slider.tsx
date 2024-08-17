d7de803a29c62c81cd20f60336a0c4333551eb05
barton05 <bartonchik05@gmail.com>
2024-08-17 17:32:58 +0800

    103 slider

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -34,34 +34,48 @@ const Slider = () => {
   const [current, setCurrent] = useState(0);
 
   return (
-    <div className="h-[calc(100vh-80)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000"></div>
-      {slides.map((slide) => (
+    <div className="h-[calc(100vh-80px)] overflow-hidden">
+      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+        {slides.map((slide) => (
+          <div
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
+            {/* Text */}
+            <div className="h-1/2 xl:w-1/2 flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl ">
+                {slide.description}
+              </h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
+              <Link href={slide.url}>
+                <button className="rounded-md bg-black text-white py-3 px-4">
+                  Shop Now
+                </button>
+              </Link>
+            </div>
+            {/* Image */}
+            <div className="h-1/2 xl:w-1/2 relative">
+              <Image
+                src={slide.img}
+                alt=""
+                fill
+                sizes="100%"
+                className="object-cover"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {slides.map((slide, index) => (
         <div
-          className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+          className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+            current === index ? "scale-150" : ""
+          } `}
           key={slide.id}
-        >
-          {/* Text */}
-          <div className="h-1/2 xl:w-1/2 ">
-            <h2>{slide.description}</h2>
-            <h1>{slide.title}</h1>
-            <Link href={slide.url}>
-              <button className="rounded-md bg-black text-white">
-                Shop Now
-              </button>
-            </Link>
-          </div>
-          {/* Image */}
-          <div className="h-1/2 xl:w-1/2 relative">
-            <Image
-              src={slide.img}
-              alt=""
-              fill
-              sizes="100%"
-              className="object-cover"
-            />
-          </div>
-        </div>
+        ></div>
       ))}
     </div>
   );
