92f37901719fe0aeab0db28db0e6bdbeeae865a8
barton05 <bartonchik05@gmail.com>
2024-08-18 21:06:54 +0800

    153 customize page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,15 +1,58 @@
+import Add from "../components/Add";
+import CustomizeProduct from "../components/CustomizeProducts";
 import ProductImages from "../components/ProductImages";
 
 const SinglePage = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      {/* Img */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages />
-        Images
       </div>
-      {/* Text */}
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">Text</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <div className="text-4xl font-medium">Product Name</div>
+        <p className="text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          unde eligendi, tempora veniam repellat consequuntur praesentium
+          eveniet quae sint numquam reiciendis qui animi. Harum sunt quam
+          recusandae qui autem? Enim!
+        </p>
+        <div className="h-[2px] bg-gray-100 " />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-grey-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100"> </div>
+        <CustomizeProduct />
+        <Add />
+        <div className="h-[2px] bg-gray-100"> </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+            voluptate alias laborum at?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+            voluptate alias laborum at?
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+            voluptate alias laborum at?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/components/CustomizeProducts.tsx
+++ src/app/components/CustomizeProducts.tsx
@@ -1,14 +1,21 @@
-import ProductImages from "./ProductImages";
-
-const SinglePage = () => {
+const CustomizeProduct = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max"></div>
-      <ProductImages />
-      <div className="w-full lg:w-1/2 flex flex-col gap-6"></div>
-      <div className="text-4xl font-medium">Product Name</div>
+    <div className="flex flex-col gap-6">
+      <h4 className="font-medium">Choose a color</h4>
+      <ul className="flex items-center gap-3"></ul>
+      <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
+        <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+      </li>
+      <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500">
+        <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+      </li>
+      <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
+        <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+      </li>
+      <h4 className="font-medium"> Choose a size</h4>
+      CustomizeProducts
     </div>
   );
 };
 
-export default SinglePage;
+export default CustomizeProduct;
