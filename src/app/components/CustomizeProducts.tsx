d9dd79d35734bee9475d7a001fd0d589e7376004
barton05 <bartonchik05@gmail.com>
2024-08-18 17:03:03 +0800

    productimages

--- src/app/components/CustomizeProducts.tsx
+++ src/app/components/CustomizeProducts.tsx
@@ -4,6 +4,7 @@ const SinglePage = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max"></div>
+      <ProductImages />
       <div className="w-full lg:w-1/2 flex flex-col gap-6"></div>
       <div className="text-4xl font-medium">Product Name</div>
     </div>
