8c485c5ff23bae4e562d33de66ba83ea16a3a40f
barton05 <bartonchik05@gmail.com>
2024-08-19 22:08:49 +0800

    waiting fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { WixClientContext } from "@/context/wixContext";
 import CategoryList from "./components/CategoryList";
 import NewProductList from "./components/NewProductList";
 import ProductList from "./components/ProductList";
 import Slider from "./components/Slider";
+import { useContext, useEffect } from "react";
 
 const HomePage = () => {
+  const wixClient = useContext(WixClientContext);
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await wixClient.products.queryProducts().find();
+      console.log(res);
+    };
+    getProducts();
+  },[wixClient]);
   return (
     <div className="">
       <Slider />
