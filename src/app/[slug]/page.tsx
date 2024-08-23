06e8efd335034b14f139ed7c75712918ca5bd823
barton05 <bartonchik05@gmail.com>
2024-08-23 15:38:01 +0800

    single product page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -11,63 +11,64 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
     .eq("slug", params.slug)
     .find();
 
-  console.log(products.items[0]);
-  if (!products.items[0]) {
-    return notFound();
-  }
+    if (!products.items[0]) {
+      return notFound();
+    }
 
-  return (
-    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
-      </div>
-      <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <div className="text-4xl font-medium">{products.items[0].name}</div>
-        <p className="text-gray-500">{products.items[0].description}</p>
-        <div className="h-[2px] bg-gray-100 " />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-grey-500 line-through">
-            {products.items[0].convertedPriceData?.formatted?.price}
-          </h3>
-          <h2 className="font-medium text-2xl">
-            {products.items[0].convertedPriceData?.formatted?.discountedPrice}
-          </h2>
+    const product = products.items[0];
+    console.log(product.media?.items);
+    return (
+      <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+        <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+          <ProductImages productMediaItems={product.media?.items} />
         </div>
-        <div className="h-[2px] bg-gray-100"> </div>
-        <CustomizeProduct />
-        <Add />
+        <div className="w-full lg:w-1/2 flex flex-col gap-6">
+          <div className="text-4xl font-medium">{product.name}</div>
+          <p className="text-gray-500">{product.description}</p>
+          <div className="h-[2px] bg-gray-100 " />
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-grey-500 line-through">
+              {product.convertedPriceData?.formatted?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              {product.convertedPriceData?.formatted?.discountedPrice}
+            </h2>
+          </div>
+          <div className="h-[2px] bg-gray-100"> </div>
+          <CustomizeProduct />
+          <Add />
 
-        <div className="h-[2px] bg-gray-100"> </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
-            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
-            voluptate alias laborum at?
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
-            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
-            voluptate alias laborum at?
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
-            ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
-            voluptate alias laborum at?
-          </p>
+          <div className="h-[2px] bg-gray-100"> </div>
+          <div className="text-sm">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+              ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+              voluptate alias laborum at?
+            </p>
+          </div>
+          <div className="text-sm">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+              ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+              voluptate alias laborum at?
+            </p>
+          </div>
+          <div className="text-sm">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              explicabo in dolor ducimus iusto! Quas, voluptas adipisci saepe
+              ipsum sit suscipit vero ipsam voluptatibus molestias pariatur
+              voluptate alias laborum at?
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default SinglePage;
--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -1,31 +1,29 @@
 "use client";
 
+import type { products } from "@wix/stores";
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-  { id: 1000, url: "/woman.png" },
-  { id: 2000, url: "/skrill.png" },
-  { id: 3000, url: "/youtube.png" },
-];
-
-const ProductImages = () => {
+const ProductImages = ({ productMediaItems }: { productMediaItems: any }) => {
   const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          src={productMediaItems[index].image.url}
           alt=""
           fill
           sizes="50vw"
           className="object-cover rounded-md"
         />
       </div>
-      {images.map((img, i) => (
-        <div className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer" key={img.id}>
+      {productMediaItems.map((item: any, i: number) => (
+        <div
+          className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+          key={item.id}
+        >
           <Image
-            src={img.url}
+            src={item.image.url}
             alt=""
             fill
             sizes="30vw"
